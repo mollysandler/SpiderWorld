15facd06c80c8210a2a4a7ca5fb2cf5e51b52510
jemmr <jarona@calpoly.edu>
2024-01-29 21:19:15 -0800

    Added diamond drawing.
    Make sure main PApplet declares a world and calls:
    ` this.world = new World( this ); `
    in setup()
    ` world.drawWorld() `
    in draw()
    ` world.setLevel( /* HashMap of level contents */ )`
    somewhere before each level

--- World.java
+++ World.java
@@ -1,4 +1,6 @@
 import processing.core.PApplet;
+import java.util.HashMap;
+import java.util.List;
 
 public final class World {
     public PApplet screen;
@@ -6,36 +8,67 @@ public final class World {
     public int topPadding = 143;
     public int numRows;
     public int tileWidth;
-    public String currentLevel;
+    public int bgColor;
+    public HashMap< String, List< Point > > levelMap;
 
     public World( PApplet screen ) {
         this.screen = screen;
     }
 
-    public void setLevel(){
-//        assign JSON of current problem to currentLevel
+    public void setLevel( HashMap< String, List< Point > > level ) {
+        levelMap = level;
     }
 
     public void readLevel() {
-//        read currentLevel
+//        edit if level has different size or background color
         numRows = 5;
         tileWidth = 60;
+        bgColor = screen.color( 110, 113, 132 );
     }
 
     public void drawGrid() {
-        screen.fill( screen.color( 110, 113, 132 ) );
+        screen.fill( bgColor );
         screen.stroke( 204, 204, 204 );
         screen.rect( leftPadding, topPadding, tileWidth*numRows, tileWidth*numRows );
-        for (int row = 1; row < numRows; row++) {
+        for ( int row = 1; row < numRows; row++ ) {
             screen.line( leftPadding, topPadding + tileWidth*row, leftPadding + tileWidth*numRows, topPadding + tileWidth*row );
         }
-        for (int col = 1; col < numRows; col++) {
+        for ( int col = 1; col < numRows; col++ ) {
             screen.line( leftPadding + tileWidth*col, topPadding, leftPadding + tileWidth*col, topPadding + tileWidth*numRows );
         }
     }
 
     public void drawEntities() {
-        screen.image( screen.loadImage( "images/spider.png" ), leftPadding, topPadding );
+        screen.image( screen.loadImage( "images/spider_east.png" ), leftPadding, topPadding );
+        screen.textSize( 12 );
+
+        // draw red diamonds
+        if ( levelMap.containsKey( "red" ) ) {
+            screen.fill( screen.color( 255, 89, 94 ) );
+            for ( Point P : levelMap.get( "red" ) ) {
+                float diamondX = (float) ( leftPadding + tileWidth*( P.getX() + .5 ) - 5 );
+                float diamondY = (float) ( topPadding + tileWidth*( P.getY() + .5 ) + 5 );
+                screen.text('◆', diamondX, diamondY );
+            }
+        }
+        // draw blue diamonds
+        if ( levelMap.containsKey( "blue" ) ) {
+            screen.fill( screen.color( 63, 166, 231 ) );
+            for ( Point P : levelMap.get( "blue" ) ) {
+                float diamondX = (float) ( leftPadding + tileWidth*( P.getX() + .5 ) - 5 );
+                float diamondY = (float) ( topPadding + tileWidth*( P.getY() + .5 ) + 5 );
+                screen.text('◆', diamondX, diamondY );
+            }
+        }
+        // draw green diamonds
+        if ( levelMap.containsKey( "green" ) ) {
+            screen.fill( screen.color( 138, 201, 38 ) );
+            for ( Point P : levelMap.get( "green" ) ) {
+                float diamondX = (float) ( leftPadding + tileWidth*( P.getX() + .5 ) - 5 );
+                float diamondY = (float) ( topPadding + tileWidth*( P.getY() + .5 ) + 5 );
+                screen.text('◆', diamondX, diamondY );
+            }
+        }
     }
 
     public void drawWorld() {
