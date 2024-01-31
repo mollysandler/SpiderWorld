c39cebcbe2729449e82f52f7adb97a2ba7fafaa7
Molly Sandler <74861421+mollysandler@users.noreply.github.com>
2024-01-30 16:07:40 -0800

    remove sonarlint warnigns

--- Driver.java
+++ Driver.java
@@ -4,45 +4,32 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Driver extends PApplet{
-    public static World world;
-    public LoadLevels level = new LoadLevels(0);
+    private World world;
+    private LoadLevels level;
 
+    @Override
     public void settings(){
         size(700, 700);
     }
 
+    @Override
     public void setup(){
         background(255, 255, 255);
         world = new World(this);
+        level = new LoadLevels(0);
     }
 
+    @Override
     public void draw() {
-        //System.out.println(level.getLevel());
 
         HashMap<String, ArrayList<Point>> map = level.loadHashMap();
         world.setLevel(map);
         world.drawWorld();
-/*
-        Point bluePoint1 = new Point(1, 0);
-        Point greenPoint1 = new Point(3, 3);
-        Point redPoint1 = new Point(2, 2);
-        ArrayList<Point> bluePoints = new ArrayList<>();
-        bluePoints.add(bluePoint1);
-        ArrayList<Point> greenPoints = new ArrayList<>();
-        greenPoints.add(greenPoint1);
-        ArrayList<Point> redPoints = new ArrayList<>();
-        redPoints.add(redPoint1);
-        HashMap<String, ArrayList<Point>> map = new HashMap<>();
-        map.put("blue", bluePoints);
-        map.put("green", greenPoints);
-        map.put("red", redPoints);
-*/
         level.saveHashMap(map);
-       // HashMap<String, ArrayList<Point>> loadedMap = level.loadHashMap();
     }
     public static void main(String[] args) {
         String[] processingArgs = {"Driver"};
-        Driver Driver = new Driver();
-        PApplet.runSketch(processingArgs, Driver);
+        Driver running = new Driver();
+        PApplet.runSketch(processingArgs, running);
     }
 }
