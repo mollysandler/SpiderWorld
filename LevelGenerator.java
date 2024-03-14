bd5a9746c9da844475a58b515a1e6d4177c6fe27
Aayush <ajoshi16@calpoly.edu>
2024-03-14 15:10:50 -0700

    Completion of Level Generator

--- LevelGenerator.java
+++ LevelGenerator.java
@@ -17,7 +17,11 @@ public class LevelGenerator {
         level12();
         level13();
         level14();
-        level15();
+        level16();
+        level17();
+        level18();
+        level19();
+        level20();
     }
 
     public static void level1() {
@@ -611,6 +615,194 @@ public class LevelGenerator {
         redPoints.add(redPoint4);
 
         ArrayList<Point> greenPoints = new ArrayList<>();
+
+        ArrayList<Point> spider = new ArrayList<>();
+        spider.add(spiderLoc);
+        spider.add(spiderDirection);
+        ArrayList<Point> dimentions = new ArrayList<>();
+        dimentions.add(rowsXcols);
+
+        // Create map and add ArrayLists
+        HashMap<String, ArrayList<Point>> map = new HashMap<>();
+        map.put("blue", bluePoints);
+        map.put("red", redPoints);
+        map.put("green", greenPoints);
+        map.put("spider", spider);
+        map.put("dimentions", dimentions);
+
+        lvl.saveHashMap(map);
+    }
+    public static void level16() {
+        LoadLevels lvl = new LoadLevels(16);
+
+        // Create points
+        Point redPoint1 = new Point(3, 1);
+        Point bluePoint1 = new Point(1, 3);
+        Point greenPoint1 = new Point(4, 2);
+
+        Point spiderLoc = new Point(2, 2);
+        Point spiderDirection = new Point(3, 0);
+        Point rowsXcols = new Point(5, 5);
+
+        // Create ArrayLists and add points
+        ArrayList<Point> bluePoints = new ArrayList<>();
+        bluePoints.add(bluePoint1);
+
+        ArrayList<Point> redPoints = new ArrayList<>();
+        redPoints.add(redPoint1);
+
+        ArrayList<Point> greenPoints = new ArrayList<>();
+        greenPoints.add(greenPoint1);
+
+        ArrayList<Point> spider = new ArrayList<>();
+        spider.add(spiderLoc);
+        spider.add(spiderDirection);
+        ArrayList<Point> dimentions = new ArrayList<>();
+        dimentions.add(rowsXcols);
+
+        // Create map and add ArrayLists
+        HashMap<String, ArrayList<Point>> map = new HashMap<>();
+        map.put("blue", bluePoints);
+        map.put("red", redPoints);
+        map.put("green", greenPoints);
+        map.put("spider", spider);
+        map.put("dimentions", dimentions);
+
+        lvl.saveHashMap(map);
+    }
+    public static void level17() {
+        LoadLevels lvl = new LoadLevels(17);
+
+        // Create points
+        Point redPoint1 = new Point(1, 2);
+        Point bluePoint1 = new Point(0, 1);
+        Point redPoint2 = new Point(2, 0);
+
+        Point spiderLoc = new Point(2, 2);
+        Point spiderDirection = new Point(0, 0);
+        Point rowsXcols = new Point(3, 3);
+
+        // Create ArrayLists and add points
+        ArrayList<Point> bluePoints = new ArrayList<>();
+        bluePoints.add(bluePoint1);
+
+        ArrayList<Point> redPoints = new ArrayList<>();
+        redPoints.add(redPoint1);
+        redPoints.add(redPoint2);
+
+        ArrayList<Point> greenPoints = new ArrayList<>();
+
+        ArrayList<Point> spider = new ArrayList<>();
+        spider.add(spiderLoc);
+        spider.add(spiderDirection);
+        ArrayList<Point> dimentions = new ArrayList<>();
+        dimentions.add(rowsXcols);
+
+        // Create map and add ArrayLists
+        HashMap<String, ArrayList<Point>> map = new HashMap<>();
+        map.put("blue", bluePoints);
+        map.put("red", redPoints);
+        map.put("green", greenPoints);
+        map.put("spider", spider);
+        map.put("dimentions", dimentions);
+
+        lvl.saveHashMap(map);
+    }
+    public static void level18() {
+        LoadLevels lvl = new LoadLevels(18);
+
+        // Create points
+        Point redPoint1 = new Point(1, 0);
+        Point greenPoint1 = new Point(1, 1);
+
+        Point spiderLoc = new Point(0, 0);
+        Point spiderDirection = new Point(2, 0);
+        Point rowsXcols = new Point(2, 2);
+
+        // Create ArrayLists and add points
+        ArrayList<Point> bluePoints = new ArrayList<>();
+
+        ArrayList<Point> redPoints = new ArrayList<>();
+        redPoints.add(redPoint1);
+
+        ArrayList<Point> greenPoints = new ArrayList<>();
+        greenPoints.add(greenPoint1);
+
+        ArrayList<Point> spider = new ArrayList<>();
+        spider.add(spiderLoc);
+        spider.add(spiderDirection);
+        ArrayList<Point> dimentions = new ArrayList<>();
+        dimentions.add(rowsXcols);
+
+        // Create map and add ArrayLists
+        HashMap<String, ArrayList<Point>> map = new HashMap<>();
+        map.put("blue", bluePoints);
+        map.put("red", redPoints);
+        map.put("green", greenPoints);
+        map.put("spider", spider);
+        map.put("dimentions", dimentions);
+
+        lvl.saveHashMap(map);
+    }
+    public static void level19() {
+        LoadLevels lvl = new LoadLevels(19);
+
+        // Create points
+        Point redPoint1 = new Point(1, 0);
+        Point greenPoint1 = new Point(1, 1);
+        Point greenPoint2 = new Point(1, 2);
+
+        Point spiderLoc = new Point(0, 0);
+        Point spiderDirection = new Point(2, 0);
+        Point rowsXcols = new Point(3, 3);
+
+        // Create ArrayLists and add points
+        ArrayList<Point> bluePoints = new ArrayList<>();
+
+        ArrayList<Point> redPoints = new ArrayList<>();
+        redPoints.add(redPoint1);
+
+        ArrayList<Point> greenPoints = new ArrayList<>();
+        greenPoints.add(greenPoint1);
+        greenPoints.add(greenPoint2);
+
+        ArrayList<Point> spider = new ArrayList<>();
+        spider.add(spiderLoc);
+        spider.add(spiderDirection);
+        ArrayList<Point> dimentions = new ArrayList<>();
+        dimentions.add(rowsXcols);
+
+        // Create map and add ArrayLists
+        HashMap<String, ArrayList<Point>> map = new HashMap<>();
+        map.put("blue", bluePoints);
+        map.put("red", redPoints);
+        map.put("green", greenPoints);
+        map.put("spider", spider);
+        map.put("dimentions", dimentions);
+
+        lvl.saveHashMap(map);
+    }
+    public static void level20() {
+        LoadLevels lvl = new LoadLevels(20);
+
+        // Create points
+        Point redPoint1 = new Point(1, 0);
+        Point greenPoint1 = new Point(1, 1);
+        Point greenPoint2 = new Point(2, 2);
+
+        Point spiderLoc = new Point(0, 0);
+        Point spiderDirection = new Point(3, 0);
+        Point rowsXcols = new Point(3, 3);
+
+        // Create ArrayLists and add points
+        ArrayList<Point> bluePoints = new ArrayList<>();
+
+        ArrayList<Point> redPoints = new ArrayList<>();
+        redPoints.add(redPoint1);
+
+        ArrayList<Point> greenPoints = new ArrayList<>();
+        greenPoints.add(greenPoint1);
+        greenPoints.add(greenPoint2);
 
         ArrayList<Point> spider = new ArrayList<>();
         spider.add(spiderLoc);
